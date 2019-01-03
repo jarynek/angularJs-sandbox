6bc910ce1730033d3425a12ea41efd8e381de6db
Jarek <spurny.jarek@seznam.cz>
2019-01-03 05:40:53 +0100

    master -> events js

--- ts/events.ts
+++ ts/events.ts
@@ -6,6 +6,10 @@ angularEvents.module('appEvents', [filterEvents])
         $scope.title = 'Events AngularJs';
 
         $scope.dragInit = false;
+        $scope.dragg = {
+          left:null,
+          top:null
+        };
 
         /**
          * Lists
@@ -77,19 +81,26 @@ angularEvents.module('appEvents', [filterEvents])
                     item.addEvent = false;
                 }
             });
-        }
+        };
 
         /**
          * dragEventInit
          */
         $scope.dragEventInit = function (event: any) {
 
-            $scope.dragInit = true;
             let el = event.target;
             let clone = el.closest('.draggable').cloneNode(true);
             let body = document.getElementsByTagName('body');
 
-            clone.classList.add('cloned');
+            if(!el){
+                return;
+            }
+
+            $scope.dragInit = true;
+            $scope.dragg.left = event.pageX - el.getBoundingClientRect().left;
+            $scope.dragg.top = event.pageY - el.getBoundingClientRect().top;
+
+            clone.classList.add('cloned', 'hdn');
             body[0].classList.add('lock');
 
             body[0].appendChild(clone);
@@ -100,14 +111,43 @@ angularEvents.module('appEvents', [filterEvents])
          * @param {object} event
          */
         $scope.dragEventDestroy = function (event: any) {
+
             $scope.dragInit = false;
+
+            let el = event.target;
+            let target = el.closest('.list') ? el.closest('.list') : null;
             let cloned = document.getElementsByClassName('cloned');
             let body = document.getElementsByTagName('body');
 
+            /**
+             * Add event to list
+             */
+            if(target && cloned){
+                let id = target.getAttribute('data-list');
+
+                $scope.lists.filter((item:any)=>{
+                   if(item.id == id){
+                       item.events.push({title: 'sdfds'});
+                       console.log(item);
+                       console.log(cloned);
+                   }
+                });
+            }
+
+            /**
+             * Remove cloned
+             */
             if (cloned.length > 0) {
                 cloned[0].remove();
             }
+
             body[0].classList.remove('lock');
+
+            $scope.dragg.left = null;
+            $scope.dragg.top = null;
+            el = null;
+            target = null;
+            body = null;
         };
 
         /**
@@ -115,13 +155,33 @@ angularEvents.module('appEvents', [filterEvents])
          * @param {object} event
          */
         $scope.dragMove = function (event: any) {
-            let list = event.target;
+
+            let el = event.target;
 
             if ($scope.dragInit == true) {
-                let cloned = document.getElementsByClassName('cloned');
+                let cloned = document.getElementsByClassName('cloned')[0];
 
-                if(cloned[0]){
-                    cloned[0].setAttribute('style', 'left:' + event.pageX + 'px; top: ' + event.pageY + 'px');
+                if(cloned){
+                    let left = (event.pageX - $scope.dragg.left);
+                    let top = (event.pageY - $scope.dragg.top);
+                    let target = el.closest('.list') ? el.closest('.list') : null;
+
+                    cloned.classList.remove('hdn');
+                    cloned.setAttribute('style', 'left:' + left + 'px; top: ' + top + 'px');
+
+                    /**
+                     * Remove class active
+                     */
+                    document.querySelectorAll('.active').forEach(function(item){
+                        item.classList.remove('active');
+                    });
+
+                    if(target !== null){
+                        let listId = target.getAttribute('data-list');
+
+                        target.classList.add('active');
+                        console.log(listId);
+                    }
                 }
             }
         };
